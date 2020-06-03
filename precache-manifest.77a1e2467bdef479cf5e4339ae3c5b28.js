aba5652472a7ded1844d6fdd39869cef10604f7c
Kris <sichinava.ch@gmail.com>
2020-06-03 17:31:14 +0300

    Updates

---
+++ precache-manifest.77a1e2467bdef479cf5e4339ae3c5b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda97e068b93d506e4a8b41495241da6",
+    "url": "/library-react/index.html"
+  },
+  {
+    "revision": "f0547565bff6aa919cb6",
+    "url": "/library-react/static/css/main.f310ec7a.chunk.css"
+  },
+  {
+    "revision": "6b498a3dc978445e86aa",
+    "url": "/library-react/static/js/2.5c71c898.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/library-react/static/js/2.5c71c898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0547565bff6aa919cb6",
+    "url": "/library-react/static/js/main.3247a05e.chunk.js"
+  },
+  {
+    "revision": "b50e4631cfc71f8465cd",
+    "url": "/library-react/static/js/runtime-main.21c81ac7.js"
+  }
+]);
